e26153c9407acdd57a2df4ede72cef4798332b13
ramiibm <rami.ben-maachia@outlook.fr>
2019-11-28 15:32:04 +0800

    FInish Pandas Seaborn Tuto

---
+++ D3JS/bar_chart_tuto/script.js
@@ -0,0 +1,25 @@
+var data = [4, 8, 15, 16, 23, 42];
+
+// d3.select(".chart")
+//   .selectAll("div")
+//     .data(data)
+//   .enter().append("div")
+//     .style("width", function(d) { return d * 10 + "px"; })
+//     .text(function(d) { return d; });
+
+var chart = d3.select(".chart");
+var bar = chart.selectAll("div");
+var barUpdate = bar.data(data);
+var barEnter = barUpdate.enter().append("div");
+
+// var x = d3.scale.linear()
+//     .domain([0, d3.max(data)])
+//     .range([0, 420]);
+
+barEnter.style("width", function(d) {
+	return 10 * d + "px";
+});
+
+barEnter.text(function(d) {
+	return d;
+});
