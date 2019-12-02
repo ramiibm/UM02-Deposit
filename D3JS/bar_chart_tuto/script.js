6b2b01d735e2dbab6636556184f028145f5760f1
ramiibm <rami.ben-maachia@outlook.fr>
2019-12-02 16:28:24 +0800

    FInish part 1 of first bar chart tuto
    Change scale.linear() to scaleLinear()

--- D3JS/bar_chart_tuto/script.js
+++ D3JS/bar_chart_tuto/script.js
@@ -1,23 +1,16 @@
 var data = [4, 8, 15, 16, 23, 42];
 
-// d3.select(".chart")
-//   .selectAll("div")
-//     .data(data)
-//   .enter().append("div")
-//     .style("width", function(d) { return d * 10 + "px"; })
-//     .text(function(d) { return d; });
-
 var chart = d3.select(".chart");
 var bar = chart.selectAll("div");
 var barUpdate = bar.data(data);
 var barEnter = barUpdate.enter().append("div");
 
-// var x = d3.scale.linear()
-//     .domain([0, d3.max(data)])
-//     .range([0, 420]);
+var x = d3.scaleLinear()
+	.domain([0, d3.max(data)])
+	.range([0, 420]);
 
 barEnter.style("width", function(d) {
-	return 10 * d + "px";
+	return x(d) + "px";
 });
 
 barEnter.text(function(d) {
