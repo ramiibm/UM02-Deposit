b674a1ed2dddb642363830fa7223e9b0b2342048
ramiibm <rami.ben-maachia@outlook.fr>
2019-12-24 00:30:48 +0800

    Draw graph & display fixed lines

--- Project/Interactive/script.js
+++ Project/Interactive/script.js
@@ -1,14 +1,8 @@
 d3.csv('https://raw.githubusercontent.com/ramiibm/UM02-Deposit/master/Project/Interactive/data/data.csv').then(data => draw(data));
 
 function draw(data) {
-	 console.log('draw');
 	
-	const margin = {
-		top: 20,
-		right: 20,
-		bottom: 50,
-		left: 50
-	};
+	const margin = { top: 20, right: 20, bottom: 50, left: 50 };
 	const width = 750 - margin.left - margin.right;
 	const height = 420 - margin.top - margin.bottom;
 
@@ -17,12 +11,7 @@ function draw(data) {
 	const y = d3.scaleLinear().range([height, 0]);
 
 	const colorScale = d3.scaleOrdinal()
-    .range([
-      '#4c78a8',
-      '#9ecae9',
-      '#f58518',
-      '#ffbf79',
-    ]);
+    	.range(['#4c78a8', '#9ecae9', '#f58518', '#ffbf79']);
 
 	const svg = d3.select('.chart')
 		.append('svg')
@@ -34,29 +23,27 @@ function draw(data) {
 	// data.forEach(d => {
 	// 	d.YearInNBA
 	// });
-
-	x.domain(d3.extent(data, d => d.YearInNBA));
-	y.domain([0, d3.max(data, d => d.PPG)]);
+	console.log("PPG: " + d3.max(data, d => +d.YearInNBA));
+	x.domain(d3.extent(data, d => +d.YearInNBA));
+	y.domain([0, d3.max(data, d => +d.PPG)]);
 	colorScale.domain(d3.map(data, d => d.name).keys());
 	
 	const xAxis = d3.axisBottom(x)
 		.ticks((width + 2) / (height + 2) * 5)
-		.tickSize(-height - 6)
+		.tickSize( - height - 6)
 		.tickPadding(10);
 
 	const yAxis = d3.axisRight(y)
 		.ticks(5)
-		.tickSize(7 + width)
-		.tickPadding(-15 - width)
-		.tickFormat(d => d + '');
+		.tickSize( width)
+		.tickPadding(-15 - width);
 
 	svg.append('g')
 		.attr('class', 'axis x-axis')
-		.attr("transform", `translate(0,${ height + 6 })`)
+		.attr("transform", `translate(0,${ height })`)
 		.call(xAxis);
 
 	svg.append('g')
-		.attr("transform", "translate(-7, 0)")
 		.attr('class', 'axis y-axis')
 		.call(yAxis);
 
@@ -66,4 +53,50 @@ function draw(data) {
 
 	svg.append('g')
 		.call(d3.axisLeft(y).ticks(0));
+
+	const nestByPlayerName = d3.nest()
+	  	.key(d => d.name)
+	  	.entries(data);
+
+	const players = {};
+
+	d3.map(data, d => d.name)
+		.keys()
+		.forEach(function (d, i) {
+			players[d] = nestByPlayerName[i].values;
+		});
+
+	const playerIds = Object.keys(players);
+
+	const lineGenerator = d3.line()
+		.x(d => x(d.YearInNBA))
+		.y(d => y(d.PPG));
+
+	svg
+		.selectAll('.line')
+		.data(playerIds)
+		.enter()
+		.append('path')
+		.attr('class', 'line')
+		.attr('id', playerId => `player-${ playerId }`)
+		.attr('d', playerId => lineGenerator(players[playerId]))
+		.style('stroke', playerId => colorScale(playerId));
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
