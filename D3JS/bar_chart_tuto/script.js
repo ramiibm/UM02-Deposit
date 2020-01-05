ab31d16caf0bf206cf12352cb1c0b72ff3b34efd
ramiibm <rami.ben-maachia@outlook.fr>
2020-01-06 02:00:47 +0800

    use chaining methods

--- D3JS/bar_chart_tuto/script.js
+++ D3JS/bar_chart_tuto/script.js
@@ -1,18 +1,12 @@
-var data = [4, 8, 15, 16, 23, 42];
+// var data = [4, 8, 15, 16, 23, 42];
 
-var chart = d3.select(".chart");
-var bar = chart.selectAll("div");
-var barUpdate = bar.data(data);
-var barEnter = barUpdate.enter().append("div");
+// var x = d3.scaleLinear()
+// 	.domain([0, d3.max(data)])
+// 	.range([0, 420]);
 
-var x = d3.scaleLinear()
-	.domain([0, d3.max(data)])
-	.range([0, 420]);
-
-barEnter.style("width", function(d) {
-	return x(d) + "px";
-});
-
-barEnter.text(function(d) {
-	return d;
-});
+// d3.select(".chart")
+// 	.selectAll("div")
+// 		.data(data)
+// 	.enter().append("div")
+// 		.style("width", function(d) { return x(d) + "px"; })
+// 		.text(function(d) { return d; });
